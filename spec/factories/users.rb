292a8cafe76cb9a47c466bff98bd9ed0afc0f77b
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-04-01 14:21:31 +0900

    テスココード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'furima太郎'}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {'太郎'}
+    last_name             {'山田'}
+    first_name_kana       {'タロウ'}
+    last_name_kana        {'ヤマダ'}
+    birthday              {'2000-01-01'}
+  end
+end
