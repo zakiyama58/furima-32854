906bad70bd585b94aff84ff8799292c13c127ec7
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-04-01 11:12:48 +0900

    新規登録実装の追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,19 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birthday, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
   validates :first_name
   validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  
   has_many :items
   has_many :buyers
 end
