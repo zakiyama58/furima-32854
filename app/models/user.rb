fee25c642ec57686303cd5cbf11d298e4ce8e971
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-04-01 10:33:32 +0900

    ログイン・ログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  validates :first_name
+  validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  has_many :items
+  has_many :buyers
 end
