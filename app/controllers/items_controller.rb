3c9a8661a53c17081deb7281c6c25a5c0bf1680a
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-04-02 09:08:49 +0900

    出品機能実装(セレクト)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(items_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:name, :description, :price,:category_id,:condition_id,:delivery_charge_id, :area_id, :days_to_ship_id)
+  end
+
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,26 @@
 class Item < ApplicationRecord
-  with_options presence: true, do
+  with_options presence: true do
     validates :name
     validates :description
     validates :price
   end
   
   belongs_to :user
-  has_one :buyer dependent: :destroy
+  has_one :buyer
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true, numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :area_id
+    validates :days_to_ship_id
+  end
+
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :delivery_charge_id
+  belongs_to :area_id
+  belongs_to :days_to_ship_id
 
 end
