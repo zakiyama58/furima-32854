4a61fe9890e0ff2d979662c52c560ef9758d8b19
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-04-02 20:52:09 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name               {"海"}
+    description       {"海の写真"}
+    category_id        {1}
+    condition_id       {1}
+    delivery_charge_id {1}
+    area_id            {1}
+    days_to_ship_id    {1}
+    price              {"1000"}
+    association :user
   end
 end
