2021ef155f54b827d6a52f7aa0261c14caae33ec
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-04-03 17:55:49 +0900

    商品一覧表示機能実装 修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :move_to_index, except: [:index]
 
   def index
     @items = Item.order("created_at DESC")
