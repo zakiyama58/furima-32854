f0e68592b2c4cbdaf774cafbfce287adf2735c5c
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-04-13 21:42:59 +0900

    商品購入機能実装修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id != @item.user_id
+    redirect_to root_path if current_user.id != @item.user_id || @item.buyer
   end
 
   def set_item
